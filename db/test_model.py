40ce7ca9dfb83133446a2f3e5857375346dfbdf7
STU-Idichi-Syoya <55622475+STU-Idichi-Syoya@users.noreply.github.com>
2021-09-03 13:31:12 +0900

    lintしただけ

--- db/test_model.py
+++ db/test_model.py
@@ -1,33 +1,37 @@
 from model import session
-from model import  Univercity, User,createTable,deleteTable
+from model import Univercity, User, createTable, deleteTable
 import unittest
 # from model import User, Univercity, session
+
 
 class MyTestCase1(unittest.TestCase):
 
     # Only use setUp() and tearDown() if necessary
 
     def setUp(self):
-      createTable()
-      univ = Univercity(univercityName="japan imperial Univ", domainAddr="imperial.ac.jp")
-      session.add(univ)
-      session.commit()
+        createTable()
+        univ = Univercity(univercityName="japan imperial Univ",
+                          domainAddr="imperial.ac.jp")
+        session.add(univ)
+        session.commit()
 
     def tearDown(self):
-      deleteTable()
-
+        deleteTable()
 
     def InsertUserTest(self):
         # Test feature one.
-        univ = session.query(Univercity).filter(Univercity.univercityName=="japan imperial Univ").first()
-        user = User(name="carlos", birthday=20000421, univercityId=univ.id, mailAddr="k3897@k.ac.jp", password="0421")
+        univ = session.query(Univercity).filter(
+            Univercity.univercityName == "japan imperial Univ").first()
+        user = User(name="carlos", birthday=20000421, univercityId=univ.id,
+                    mailAddr="k3897@k.ac.jp", password="0421")
         session.add(univ)
         session.commit()
-        bu=session.query(User).all().first()
-        unittest.assertTrue(bu==user)
+        bu = session.query(User).all().first()
+        unittest.assertTrue(bu == user)
     # def test_feature_two(self):
     #     # Test feature two.
     #     pass
+
 
 if __name__ == '__main__':
     unittest.main()
