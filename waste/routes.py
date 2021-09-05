87a7a9d57eaf4591ab8026b3ec171c7eee2dd0b3
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-09-05 09:03:59 +0900

    a

--- waste/routes.py
+++ waste/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template, redirect, url_for, flash, request,Flask,Response,abort ,session
-from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user,UserMixin
 from waste.forms import RegisterForm, LoginForm
+from waste.model import Item, User
 from . import wrapper
 from waste import db, login_manager,app
 
-# @app.teardown_request
-# def remove_session(ex=None):
-#     session.remove()
+@app.teardown_request
+def remove_session(ex=None):
+    #session.removeじゃなくてclearじゃないのか？
+    session.clear()
 
 # ホーム画面
 @app.route('/')
@@ -34,9 +36,11 @@ def login_page():
     if request.method=='GET':
         return render_template('login.html', form=form)
     elif request.method=='POST':
-        mailadr=request.form['mailAddr']
-        passwd=request.form['passwd']
-        user = db.FindUserByMailAddrPasswd(mailadr, passwd)
+        mailadr=form.user_name.data
+        passwd=form.password.data
+        user = wrapper.FindUserByMailAddrPasswd(mailadr, passwd)
+        print(wrapper.findAll()[0].email_address)
+        print(wrapper.findAll()[0].password_hash)
         if user==None:
             abort(Response('userNotFound', status=401))
             flash('ログイン失敗',category='danger')
@@ -55,14 +59,12 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         login_user(user_to_create)
-        flash(
-            f'Account created successfuly! You are now logged as {user_to_create.username}', category='success')
+        flash(f'Account created successfuly! You are now logged as {user_to_create.username}', category='success')
 
         return redirect(url_for('market_page'))
     if form.errors != {}:  # If there are not errors from the validations
         for err_msg in form.errors.values():
-            flash(
-                f'There was an error with creating a user: {err_msg}', category='danger')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
 
