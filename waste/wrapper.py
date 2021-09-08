10918786302db1ade16dadb5e90615dca9265293
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-09-08 19:27:56 +0900

    include修正

--- waste/wrapper.py
+++ waste/wrapper.py
@@ -1,4 +1,5 @@
-from .model import User,session
+from sqlalchemy.sql.operators import all_op
+from .model import ItemAllow, User,session,Item
 import typing
 
 def FindUserByMailAddrPasswd(email_address:str,passwd:str) ->typing.Union[User,None] :
@@ -17,7 +18,14 @@ def findAll():
 
 
 def session_add(user_to_create):
-    
     session_add(user_to_create)
     session.commit()
+    
+def findItemByWord(searchWord:str,userId:int=None):
+    # ログインしていない場合の検索は．．．？
+    if userId==None:
+        return []
+
+    return session.query(Item,User,ItemAllow).filter(Item.id==ItemAllow.item_id,
+        Item.user_id==userId,Item.item_name.like(searchWord)).all()
     
