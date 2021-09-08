1a84a0e29fee3a42df875c0b2136ec4c057e084f
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-09-08 19:28:28 +0900

    includeぶん編集

--- waste/model.py
+++ waste/model.py
@@ -1,29 +1,22 @@
-# from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.sql.sqltypes import Boolean
-from waste import login_manager
 from waste import bcrypt
 from flask_login import UserMixin
-from sqlalchemy import Table, Column, Integer, ForeignKey, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
 import os, sys
+from waste import app,db,sqliteFile
+
 sys.path.append(os.path.abspath(".."))
 
-Base=declarative_base()
-sqliteFile="sample_sqlite3"
-engine = create_engine(f'sqlite:///{sqliteFile}', echo=False)
-session = scoped_session(sessionmaker(bind=engine))
+app.config["SQLALCHEMY_DATABASE_URI"]=f'sqlite:///{sqliteFile}'
+session=db.session
 
 # ユーザ情報
-class User(Base,UserMixin):
+class User(db.Model,UserMixin):
     __tablename__= 'users'
-    id = Column(Integer(), primary_key=True, autoincrement=True)
-    user_name = Column(String(length=30), nullable=False)
-    birthday = Column(Integer(), nullable=False)
-    univercity_id = Column(Integer(),ForeignKey("univercities.id"),nullable=False)
-    email_address = Column(String(length=50), nullable=False, unique=True)
-    password_hash = Column(String(length=60), nullable=False)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(length=30), nullable=False)
+    birthday = db.Column(db.Integer(), nullable=False)
+    univercity_id = db.Column(db.Integer(),db.ForeignKey("univercities.id"),nullable=False)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
     
     @property
     def password(self):
@@ -38,50 +31,39 @@ class User(Base,UserMixin):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 # 大学情報
-class Univercity(Base):
+class Univercity(db.Model):
     __tablename__= 'univercities'
-
-    id = Column(Integer(), primary_key=True, autoincrement=True)
-    univercity_name = Column(String(), nullable=False)
-    domain_addr = Column(String(length=30), nullable=True)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    univercity_name = db.Column(db.String(), nullable=False)
+    domain_addr = db.Column(db.String(length=30), nullable=True)
 
 # 商品情報
-class Item(Base):
+class Item(db.Model):
     __tablename__= 'items'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    user_id =  db.Column(db.Integer(),db.ForeignKey("users.id"),nullable=False)
+    item_name = db.Column(db.String(length=30), nullable=False)
+    category = db.Column(db.String(length=30), nullable=False, unique=True)
+    dangerous = db.Column(db.Boolean(), nullable=False)
+    need_credential = db.Column(db.String(length=30), nullable=False)
+    expire = db.Column(db.Integer(), nullable=False)
 
-    id = Column(Integer(), primary_key=True, autoincrement=True)
-    user_id =  Column(Integer(),ForeignKey("users.id"),nullable=False)
-    item_name = Column(String(length=30), nullable=False)
-    category = Column(String(length=30), nullable=False, unique=True)
-    dangerous = Column(Boolean(), nullable=False)
-    need_credential = Column(String(length=30), nullable=False)
-    expire = Column(Integer(), nullable=False)
+# 商品公開先リスト
+class ItemAllow(db.Model):
+    __tablename__= 'items_allows'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    item_id =  db.Column(db.Integer(),db.ForeignKey("items.id"),nullable=False)
+    allow_univercity_id = db.Column(db.Integer(),db.ForeignKey("univercities.id"),nullable=False)
 
-# テーブルを作成する．dev_test=True->初期データ挿入
-def createTable(dev_test=False):
-    engine.execute('PRAGMA foreign_keys = true;')
-    Base.metadata.create_all(bind=engine)
-
-    if dev_test:
-        print('pass')
-        try:
-            univ = Univercity(univercity_name="japan imperial Univ",
-                                domain_address="abc.ac.jp")
-            session.add(univ)
-            session.commit()
-            user = User(user_name="carlos", birthday=20000421, univercity_id=univ.id,
-                        email_address="test@abc.ac.jp", password="0421")
-            session.add(user)
-            session.commit()
-        except:
-            pass
+#
 def deleteTable():
-    import os
-    session.close()
-    engine.dispose()
-    os.unlink("sample_sqlite3")
+    # import os
+    # session.close()
+    # engine.dispose()
+    # os.unlink("sample_sqlite3")
+    pass
 
-createTable(True)
+
 
 if __name__=='__main__':
-    createTable()
+    print('model create')
