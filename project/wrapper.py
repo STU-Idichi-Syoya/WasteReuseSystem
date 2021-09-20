727d17a683f6300be964f09d2bf42dceb1920d14
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-09-20 14:22:59 +0900

    wrapperファイル

---
+++ project/wrapper.py
@@ -0,0 +1,14 @@
+from project.models import Univercity, User
+from . import db
+
+def user_save(user_name,univercity_name,email_address,password1,birthday,univercity_id):
+    univ=db.session.query(Univercity).filter_by(univercity_name=univercity_name).first()
+    if univ is None:
+        univ=Univercity(univercity_name=univercity_name.data)
+        db.session.add(univ)
+        db.session.commit()
+    user_to_create = User(user_name=user_name.data,email_address=email_address.data,password=password1.data,birthday=birthday.data,univercity_id=univ.id)
+    db.session.add(user_to_create)
+    db.session.commit()
+    
+    return user_to_create
