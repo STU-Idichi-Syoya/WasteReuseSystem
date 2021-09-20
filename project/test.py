418bc74fa928337f3252d128264224e465d368a0
STU-Idichi-Syoya <e1q19006@oit.ac.jp>
2021-09-20 14:35:22 +0900

    fix test

--- project/test.py
+++ project/test.py
@@ -1,16 +1,33 @@
-from project.wrapper import user_save
+from .wrapper import user_save
 import unittest
-from .models import session
 
+from flask_sqlalchemy import SQLAlchemy, SignallingSession, orm
 
-def test_flask(self):
-  user = user_save(user_name="carlos", univercity_name="kandai", email_address="k3897@k.ac.jp",password1="0421",birthday=20000421, univercityId=self.univ.id)
-  #user_name univercity_name email_address password1 birthday univercity_id
-  session.add(user)
-  session.commit()
-  bu = session.query(user_save).all()
-  self.assertEqual(len(bu),1)
-  self.assertEqual(bu[0] ,user)
+from . import create_app
+
+class TestSignallingSession(SignallingSession):
+    def commit(self):
+        self.flush() # セッションが保持しているデータをすべてデータベースに書き込む
+        self.expire_all() # セッションが保持してるデータをクリアしデータベースより読むこむようにする
+
+class TestSQLAlchemy(SQLAlchemy):
+    def create_session(self, options):
+        return orm.sessionmaker(class_=SignallingSession, db=self, **options)
+
+app=create_app()
+db=TestSQLAlchemy(app)
+session=db.session
+
+class TestDB(unittest.TestCase):
+  
+  def test_register(self):
+    user = user_save(user_name="carlos", univercity_name="kandai", email_address="k3897@k.ac.jp",password1="0421",birthday=20000421, univercityId=self.univ.id,db=db)
+    #user_name univercity_name email_address password1 birthday univercity_id
+    session.add(user)
+    session.commit()
+    bu = session.query(user_save).all()
+    self.assertEqual(len(bu),1)
+    self.assertEqual(bu[0] ,user)
 
 if __name__ == '__main__':
     unittest.main()
--- project/wrapper.py
+++ project/wrapper.py
@@ -1,7 +1,7 @@
-from project.models import Univercity, User
+from .models import Univercity, User
 from . import db
 
-def user_save(user_name,univercity_name,email_address,password1,birthday,univercity_id):
+def user_save(user_name,univercity_name,email_address,password1,birthday,univercity_id,db=db):
     univ=db.session.query(Univercity).filter_by(univercity_name=univercity_name).first()
     if univ is None:
         univ=Univercity(univercity_name=univercity_name.data)
