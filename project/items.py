15823b95bf33401d868f37b3ad5d5ca16cd374de
STU-Idichi-Syoya <e1q19006@oit.ac.jp>
2021-09-20 22:40:42 +0900

    for debug

--- project/items.py
+++ project/items.py
@@ -1,13 +1,13 @@
+from .wrapper import item_save
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login.utils import expand_login_view
-from flask_migrate import current
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required,current_user
 from werkzeug.utils import send_file
 from werkzeug.wrappers.response import Response
-from .models import Item, ItemLike, ItemPhoto, User,ItemBlob
+from .models import Item, ItemComment, ItemLike, ItemPhoto, ItemTag, Tag, User,ItemBlob
 from . import db
 from .form import ItemAdd
+from .util import get_tags
 import io
 items_app = Blueprint('items', __name__)
 
@@ -22,39 +22,103 @@ def sendPhotoID(blob_id):
         )
     return Response(status=400)
 
-@items_app.route('items/add',methods=['POST'])
+               
+
+@items_app.route('/items/add',methods=['POST'])
 def item_post():
-    pass
-@items_app.route("/items/search")
+    item_name=request.form['item_name']
+    handing_method=request.form['handing_method']
+    place=request.form['place']
+    state=request.form['state']
+    message=request.form['message']
+    expire=request.form['expire']
+    tags=get_tags(message)
+    photos=request.files.getlist('photos')
+    if len(photos)>5 or len(photos)<1:
+        return 'Error Photo is too many'
+    # TODO 縮小処理
+    plist=[p.read()for p in photos]
+    # ret=item_save(item_name,current_user.user_id,expire,place,state,message,handing_method,tags,plist)
+    # if ret:
+    #     return 'OK'
+    # else:
+    #     return 'FALSE'
+    return render_template('item_add.html')
+    
+@items_app.route("/items/search_result")
 def searchItems():
     tag=request.args.get('tag')
     keyword=request.args.get('keyword')
-    # validation
-    if tag is not None:
-        tag=tag.replace(' ','')
-    if keyword is not None:
-        # 全角を半角にして、分割
-        keyword=keyword.replace('　',' ').split(' ').replace(' ','')
-    # find by tag
-    if len(tag)>0:
-        pass
-    # find by keyword
-    elif len(keyword)>0:
-        search_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like,Item.place,Item.user_id,Item.handing_method)\
-                                    .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
-                                    .join(ItemLike,Item.id==ItemLike.item_id)\
-                                    .join(User,Item.user_id==User.id)\
-                                    .filter_by(is_like=True,univercity_id==current_user.univercity_id).all()
-        #TODO:tag
-    
-    # render_template()
-        print(searchItems)
-    return 'OK'
 
+
+    
+    # # validation
+    # if tag is not None:
+    #     tag=tag.replace(' ','')
+    # if keyword is not None:
+    #     # 全角を半角にして、分割
+    #     keyword=keyword.replace('　',' ').split(' ').replace(' ','')
+
+    # if len(tag)>0:
+    #     #TODO:tag
+    #     if current_user.is_authenticated:
+    #         search_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like,Item.place,Item.user_id,Item.handing_method)\
+    #                                         .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                         .join(ItemTag,ItemTag.item_id==Item.id)\
+    #                                         .join(Tag,Tag.id==ItemTag.tag_id)\
+    #                                         .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                         .join(User,Item.user_id==User.id)\
+    #                                         .filter_by(is_like=True).filter_by(univercity_id==current_user.univercity_id,tag_name=tag).all()
+    #         return render_template('search_result.html',search_items=searchItems)
+            
+    #     else:
+    #         search_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like,Item.place,Item.user_id,Item.handing_method)\
+    #                                     .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                     .join(ItemTag,ItemTag.item_id==Item.id)\
+    #                                     .join(Tag,Tag.id==ItemTag.tag_id)\
+    #                                     .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                     .join(User,Item.user_id==User.id)\
+    #                                     .filter_by(is_like=True).filter_by(tag_name=tag).all()
+    return render_template('search_result.html',search_items=searchItems)
+    
+    # # find by keyword
+    # # TODO 複数ワード検索
+    # elif len(keyword)>0 and len(keyword[0])>0:
+    #     if current_user.is_authenticated:
+    #         search_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like,Item.place,Item.user_id,Item.handing_method)\
+    #                                     .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                     .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                     .join(User,Item.user_id==User.id)\
+    #                                     .filter_by(is_like=True).filter_by(univercity_id==current_user.univercity_id).all()
+    
+    #         return render_template('search_result.html',search_items=searchItems)
+    #     else:
+    #         search_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like,Item.place,Item.user_id,Item.handing_method)\
+    #                                     .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                     .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                     .join(User,Item.user_id==User.id)\
+    #                                     .filter_by(is_like=True).filter(Item.item_name.like(f'%{keyword[0]}%')).all()
+    #         return render_template('search_result.html',search_items=search_items)
+    # # クエリパラメータがなければ、検索画面
+    # return render_template('search.html')
+
+@items_app.route("/items/search")
+def r():
+    render_template("search.html")
+
+@items_app.route("/items/<id>")
+def showItem(item_id):
+    # item=db.session.query(Item).filter_by(id=item_id).first()
+    # comment=db.session.query(ItemComment).filter_by(item_id=item_id).order_by(Item.created_at.amount.asc()).all()
+    # if item is None:
+    #     return f'ERROR {item_id} Item is not exist'
+    # return render_template('item_datail.html',item=item,comment=comment)
+    return render_template('item_datail.html',item=None,comment=None)
+    
 
 
 @items_app.route("/")
-@items_app.route("/items")
+# @items_app.route("/items")
 def showItems():
     #     User.idと、UserSocial.user_idで内部結合し、
     # ユーザ全てをList型で取得する。
@@ -63,35 +127,37 @@ def showItems():
     # user_name = session.query(User, UserSocial).\
     # join(UserSocial, User.id==UserSocial.user_id).\
     # all()
-    if current_user.is_authenticated:
-        # いいねしたアイテム
-        like_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
-                                    .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
-                                    .join(ItemLike,Item.id==ItemLike.item_id)\
-                                    .join(User,Item.user_id==User.id)\
-                                    .filter_by(is_like=True,univercity_id==current_user.univercity_id).limit(10).all()
+    # if current_user.is_authenticated:
+    #     # いいねしたアイテム
+    #     like_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
+    #                                 .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                 .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                 .join(User,Item.user_id==User.id)\
+    #                                 .filter_by(is_like=True).filter_by(univercity_id==current_user.univercity_id).limit(10).all()
         
-        recommend_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
-                                    .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
-                                    .join(ItemLike,Item.id==ItemLike.item_id)\
-                                    .join(User,Item.user_id==User.id)\
-                                    .filter_by(univercity_id==current_user.univercity_id).limit(10).all()
+    #     recommend_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
+    #                                 .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                 .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                 .join(User,Item.user_id==User.id)\
+    #                                 .filter_by(univercity_id==current_user.univercity_id).limit(10).all()
         
-        #検索履歴
-        #TODO
-        search_his_items=[]
-        # new Item
-        new_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
-                                    .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
-                                    .join(ItemLike,Item.id==ItemLike.item_id)\
-                                    .join(User,Item.user_id==User.id)\
-                                    .filter_by(univercity_id==current_user.univercity_id).limit(10).order_by(Item.created_at.amount.desc()).all()
-    else:
-        recommend_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
-                                    .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
-                                    .join(ItemLike,Item.id==ItemLike.item_id)\
-                                    .join(User,Item.user_id==User.id)\
-                                    .limit(100).all()
+    #     #検索履歴
+    #     #TODO
+    #     search_his_items=[]
+    #     # new Item
+    #     new_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
+    #                                 .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                 .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                 .join(User,Item.user_id==User.id)\
+    #                                 .filter_by(univercity_id==current_user.univercity_id).limit(10).order_by(Item.created_at.amount.desc()).all()
+    #     return render_template('index.html',like_items=like_items,recommend_items=recommend_items,new_items=new_items)
         
-
-
+    # else:
+    #     recommend_items=db.session.query(Item.id,Item.item_name,ItemPhoto.URI,ItemLike.is_like)\
+    #                                 .join(ItemPhoto,ItemPhoto.item_id==Item.id)\
+    #                                 .join(ItemLike,Item.id==ItemLike.item_id)\
+    #                                 .join(User,Item.user_id==User.id)\
+    #                                 .limit(100).all()
+        
+        # return render_template('index.html',recommend_items=recommend_items)
+        return render_template('index.html',recommend_items=None)
