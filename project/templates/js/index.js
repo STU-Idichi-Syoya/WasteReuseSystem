d0d46e274c5484fdcbd3e7f16fe64249c5129548
mt-sumikko <mt.sumikko163@gmail.com>
2021-09-21 16:56:06 +0900

    アイテム詳細のテンプレ完成

--- project/templates/js/index.js
+++ project/templates/js/index.js
@@ -232,35 +232,6 @@ const swiper = new Swiper('.swiper', {
 });*/
 /*
  
-var mySwiper = new Swiper(".swiper-container", {
-    // オプション設定
-    loop: true, // ループ
-    speed: 600, // 切り替えスピード(ミリ秒)。
-    slidesPerView: 1, // １スライドの表示数
-    spaceBetween: 0, // スライドの余白(px)
-    direction: "horizontal", // スライド方向
-    effect: "fade", // スライド効果 ※ここを変更
- 
-    // スライダーの自動再生設定
-    autoplay: {
-        delay: 3000, // スライドが切り替わるまでの時間(ミリ秒)
-        stopOnLast: false, // 自動再生の停止なし
-        disableOnInteraction: true, // ユーザー操作後の自動再生停止
-    },
- 
-    // ページネーションを有効化
-    pagination: {
-        el: ".swiper-pagination",
-    },
- 
-    // ナビゲーションを有効化
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-*/
-
 /* Search Bar Fix（あるところだけ）
     -------------------------------------- */
 /*スクロールすると検索フィールドが上に固定される（実は微妙にうまく行ってない）*/
@@ -282,26 +253,3 @@ $(window).on('scroll', function () {
 });
 
 
-
-const swiper = new Swiper('.swiper-container', {
-    // スライドメニューの表示方法
-    direction: 'horizontal',
-    loop: true,
-    autoHeight: true,
-    autoplay: {
-        delay: 1000,
-    },
-    // スライドのページネーション表示
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    // 「次へ」や「戻る」のアイコン表示
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    // スクロールバーの表示
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
